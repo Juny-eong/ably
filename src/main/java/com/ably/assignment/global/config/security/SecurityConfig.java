71869d6a6753be6f8feda3c67ad8efb698ac4fc7
junyeong <adipy1470@naver.com>
2022-07-10 18:47:30 +0900

    fix typo

--- src/main/java/com/ably/assignment/global/config/security/SecurityConfig.java
+++ src/main/java/com/ably/assignment/global/config/security/SecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.CorsFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -55,7 +54,7 @@ public class SecurityConfig {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/users/signup", "/users/login", "verification/code", "/h2-console/**").permitAll()
+                .antMatchers("/users/signup", "/users/login", "/verification/code", "/h2-console/**").permitAll()
                 .anyRequest().authenticated()
 
 
