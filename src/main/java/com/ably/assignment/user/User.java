b7bcfa8d80bc905d37bc5586f5987379d4bf9d90
junyeong <adipy1470@naver.com>
2022-07-10 13:34:08 +0900

    feat: user entity 생성 / application.yml 생성

---
+++ src/main/java/com/ably/assignment/user/User.java
@@ -0,0 +1,35 @@
+package com.ably.assignment.user;
+
+
+import com.ably.assignment.user.enumerated.Gender;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String nickname;
+
+    private Long phoneNumber;
+
+    private Gender gender;
+
+
+}
---
+++ src/main/java/com/ably/assignment/user/enumerated/Gender.java
@@ -0,0 +1,7 @@
+package com.ably.assignment.user.enumerated;
+
+public enum Gender {
+    MALE,
+
+    FEMALE
+}
